d54e538f6bad1a1987676f0974129e3a95ccf067
wasserglas <wasserglas@t-online.de>
2016-12-03 17:19:54 +0100

    not nice coded version but works for now

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = require("express")();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 //makes it possible to determine if the user is on mobile or desktop
 var device = require('express-device');
@@ -11,7 +12,7 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
     if (req.device.type.toUpperCase() === "DESKTOP") {
-        res.sendFile(__dirname + '/mobileController.html');
+        res.sendFile(__dirname + '/desktopController.html');
     } else if (req.device.type.toUpperCase() === "PHONE") {
         res.sendFile(__dirname + '/mobileController.html');
     }
@@ -20,3 +21,41 @@ app.get('/', function(req, res){
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+io.on('connection', function (socket) {
+  socket.on('getSocketIdForRoom', function (data) {
+  	console.log("Request by ClientID " + data.clientId + " to get DesktopID of room " + data.roomId + ".");
+  	console.log("Returning " + dict[data.roomId] + " to " + data.clientId);
+  	io.to(data.clientId).emit('clientIdFromRoomId', { desktopClientId: dict[data.roomId]});
+  });
+  
+  socket.on('inputData', function (data) {
+  	console.log("New movement data for room with id " + data.roomId);
+  });
+  
+  socket.on('addRoom', function (data) {
+  	console.log("New room created [" + data.roomId + ", " + data.desktopClientId + "]");
+    dict[data.roomId] = data.desktopClientId;
+  });
+  
+  socket.on('newClient', function (data) {
+  	console.log("New client for room " + data.desktopClientId + " with ClientID " + data.id);
+    io.to(data.desktopClientId).emit('newClient', data);
+  });
+  
+    					
+  socket.on('newAccelerometerData', function(data) {
+    io.to(data.desktopClientId).emit('newAccelerometerData', data);				
+  });
+  
+  socket.on('movementData', function (data) {
+
+    io.to(data.desktopClientId).emit(data);
+  });
+  
+});
+
+var dict = {123456: 'asdhiahd'};
+
+
+

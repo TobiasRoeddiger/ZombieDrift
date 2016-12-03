9b5ea18dbf9dac1861c1ae4bfeca452c1871b710
wasserglas <wasserglas@t-online.de>
2016-12-03 17:22:29 +0100

    fixed port

--- server.js
+++ server.js
@@ -18,7 +18,8 @@ app.get('/', function(req, res){
     }
 });
 
-http.listen(3000, function(){
+var port = process.env.port || 1337;
+http.listen(port, function(){
   console.log('listening on *:3000');
 });
 
